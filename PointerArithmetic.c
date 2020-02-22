0dbdb81e3968bcffb7f452f52ad166bc25768bc1
Adrian Garcia <adriangarlop@hotmail.com>
2020-02-22 11:12:51 -0600

    Added PointArithmetic.c code convention

--- PointerArithmetic.c
+++ PointerArithmetic.c
@@ -1,12 +1,22 @@
+//
+// Autor: Adrian Garcia Lopez
+// Matrícula: A01351166
+// Fecha: 21/02/2020
+// 
+// Usar aritmetica de apuntadores
 #include <stdio.h>
 
+// Programa principal
 int main() {
 
+	// String que se utilizara
 	char direction[50];
 
+	// Pedimos direccion
 	scanf(" %s", direction);
 	printf("Original direction: %s\n", direction);
 
+	// Modificamos el string
 	for (int i=0; i<50; i++) {
 		
 		if (direction[i] == 'a') 
@@ -25,7 +35,9 @@ int main() {
 			direction[i] += '2' - 'u';
 	}
 
+	// Damos resultados de la nueva direccion
 	printf("New direction: %s", direction);	
 
+	// Terminamos programa
 	return 0;
 }
